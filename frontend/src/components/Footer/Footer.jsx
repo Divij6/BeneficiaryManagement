4039c2a233f92eae343e54e78f977b5c3ddf4088
konarknehete <konarknehete7@gmail.com>
2025-08-24 16:33:55 +0530

    minor change

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-blue-600 text-white py-10 mt-10">
+    <footer className="bg-blue-600 text-white py-10 ">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8">
         
         <div>
