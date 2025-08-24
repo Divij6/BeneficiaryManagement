1870b06ff5aa61aebbe3386108f03c878dc90fd2
Raghav <raghavjha619@gmail.com>
2025-08-24 15:16:25 +0530

    changed tailwind version

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
+import Dashboard from './components/Dashboard/Dashboard.jsx'
+import UserDashboard from './components/Dashboard/UserDashboard.jsx'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='/dashboard' element={<UserDashboard/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
