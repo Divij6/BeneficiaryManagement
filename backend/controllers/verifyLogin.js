ded39dd9ed54feef581f3d25dde9fd8f35173bd6
Divij <gujarathidivij@gmail.com>
2025-08-24 15:55:09 +0530

    Added verifyLogin.js

--- backend/controllers/verifyLogin.js
+++ backend/controllers/verifyLogin.js
@@ -1,28 +1,48 @@
 import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const verifyLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await userModel.findOne({ email, password });
-    if (user) {      
-      res.status(200).send({
-        message: "Login Successful",
-        success: true,
-        data: user,
+
+    
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(200).send({
+        message: "User not found",
+        success: false,
       });
-    } else {
-      res.status(200).send({
+    }
+
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(200).send({
         message: "Invalid Credentials",
         success: false,
       });
     }
-    }catch (error) {
-      console.log(error);
-      res.status(500).send({
-        message: "Error in Login",
-        success: false,
-        error,
-      });
-    }
-  }  
+
+    
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).send({
+      message: "Login Successful",
+      success: true,
+      token,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error in Login",
+      success: false,
+      error,
+    });
+  }
+};
+
 export default verifyLogin;
