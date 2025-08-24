38d5139eb62e2a43f4896ab7bd6b39dad2498251
Vydeh <142721431+Vydeh07@users.noreply.github.com>
2025-08-24 15:38:05 +0530

    register user added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoute.js'
+
 
 
 const app= express()
@@ -10,7 +13,7 @@ connectDB()
 app.use(express.json())
 app.use(cors())
 //api endpoints
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter)
 
 app.get('/',(req,res)=>{
     res.send('WORKING')
