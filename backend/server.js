e0ecc8c22968dc1a2b2b79efc565327d6385ab0d
Vydeh <142721431+Vydeh07@users.noreply.github.com>
2025-08-25 15:05:42 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoute.js'
-
-
+import adminRouter from './routes/adminRoute.js'  // This was missing
 
 const app= express()
 const port= process.env.PORT || 4000
@@ -14,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 //api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/admin', adminRouter)
 
 app.get('/',(req,res)=>{
     res.send('WORKING')
